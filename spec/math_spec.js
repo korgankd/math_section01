c05e83e2c16c28d78b80189c0899b4cbb8a73845
Kent Korgan <korgankd@dukes.jmu.edu>
2016-02-24 09:51:24 -0500

    4 specs 0 failures

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -6,3 +6,21 @@ describe("A simple program that does math", function() {
 		expect(math.add(3, 5)).toBe(8);
 	});
 });
+
+describe("A simple program that does math", function() {
+	it("can subtract two numbers", function() {
+		expect(math.subtract(3, 5)).toBe(-2);
+	});
+});
+
+describe("A simple program that does math", function() {
+	it("can multiply two numbers", function() {
+		expect(math.multiply(3, 5)).toBe(15);
+	});
+});
+
+describe("A simple program that does math", function() {
+	it("can divide two numbers", function() {
+		expect(math.divide(10, 5)).toBe(2);
+	});
+});
