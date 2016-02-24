c61a3d46c3f15e6a0eed7f09aa46a678064254ea
Kent Korgan <korgankd@dukes.jmu.edu>
2016-02-24 09:44:13 -0500

    Passing add test

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,5 +1,8 @@
+// tell the test where to find the code
+var math = require('../math');
+
 describe("A simple program that does math", function() {
 	it("can add two numbers", function() {
-		expect(add(3, 5)).toBe(8);
+		expect(math.add(3, 5)).toBe(8);
 	});
 });
